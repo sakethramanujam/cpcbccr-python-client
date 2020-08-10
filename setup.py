46c7e885e1b30214cbce23c28e6aa127bbfb5cb2
Saketha Ramanjam <ramanujamsamavedam@hotmail.com>
2020-08-10 16:38:23 +0530

    [setup] add setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+with open('README.md','r') as f:
+    readme = f.read()
+
+setup(
+    name="cpcbccr",
+    include_package_data=True,
+    packages=['cpcbccr',],
+    description="An unofficial python client to obtain cpcb ccr data",
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    version="0.1",
+    python_requires='>=3.6',
+    install_requires=['requests', 'pandas'],
+    entry_points = {"console_scripts": ['cpcbccr = cpcbccr.cpcbccr']},
+    author='Saketha Ramanujam',
+    author_email="saketh.ramanujam98@gmail.com",
+    url="https://github.com/sakethramanujam/cpcbccr-python-client",
+    classifiers=[
+         "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
